055ae01b8ed9935e6c1d9519833e07970eebbdee
Eichwald <Eichwald6@hotmail.com>
2022-01-28 12:18:17 +0100

    added testrail api test reporting

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,10 +1,22 @@
 from src.calculator import add
 import pytest
+from src.testrail import *
+
+client = APIClient('https://testautomationtest.testrail.io/')
+client.user = 'jonas.eichwald@velux.com'
+client.password = '6/FeVpnvRjU3TjdRFnkT'
 
 
 def test_add():
     result = add(3, 4)
     assert result == 7
+    res = client.send_post(
+        'add_result_for_case/20/2373',
+        {'status_id': 1, 'comment': 'This test worked fine!'}
+    )
+
+    print(res)
+
 
 
 def test_add_string():
--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -1,10 +1,21 @@
 from src.calculator import multiply
 import pytest
+from src.testrail import *
+
+client = APIClient('https://testautomationtest.testrail.io/')
+client.user = 'jonas.eichwald@velux.com'
+client.password = '6/FeVpnvRjU3TjdRFnkT'
 
 
 def test_multiply():
     result = multiply(3, 4)
     assert result == 12
+    res = client.send_post(
+        'add_result_for_case/20/2373',
+        {'status_id': 2, 'comment': 'This test did not work fine!'}
+    )
+
+    print(res)
 
 
 def test_multiply_string():
