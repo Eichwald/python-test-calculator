89b446ebd2a564fc6187788d293ba82abcd95d01
Eichwald <Eichwald6@hotmail.com>
2022-01-31 13:09:34 +0100

    selenium test

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,10 +1,27 @@
-from src.calculator import add
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from src.calculator import add
 from src.testrail import *
 
 client = APIClient('https://testautomationtest.testrail.io/')
 client.user = 'jonas.eichwald@velux.com'
 client.password = '6/FeVpnvRjU3TjdRFnkT'
+
+chrome_options = Options()
+chrome_options.binary_location = r"C:\Users\45232\Desktop\Codeing\python-test-calculator\src/chromedriver.exe"
+chrome_options.add_argument('--start-maximized')
+chrome_options.add_argument('--headless')
+
+
+def test_sele():
+    driver = webdriver.Chrome()
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+
+    driver.close()
+    driver.quit()
 
 
 def test_add():
@@ -16,7 +33,6 @@ def test_add():
     )
 
     print(res)
-
 
 
 def test_add_string():
