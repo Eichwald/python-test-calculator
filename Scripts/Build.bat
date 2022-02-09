4d5bab7041d3834e44ea285037db0d6a835e5871
Eichwald <Eichwald6@hotmail.com>
2022-02-09 10:24:37 +0100

    test

--- Scripts/Build.bat
+++
@@ -1,3 +0,0 @@
-SET cur_dir=%cd%
-echo %cur_dir%
-LabVIEWCLI -LogToConsole true -OperationName ExecuteBuildSpec -ProjectPath "%cur_dir%\LabviewJenkinsExample.lvproj" -BuildSpecName "App"
--- Scripts/UnitTest.bat
+++
@@ -1,3 +0,0 @@
-SET cur_dir=%cd%
-echo %cur_dir%
-LabVIEWCLI -LogToConsole true -OperationName RunVI -VIPath "%cur_dir%\tests\JenkinsUnitTests.vi"
