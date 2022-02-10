f0f2448fead6d3e624123b98b4ed77b7f8b994af
Eichwald <eichwald6@hotmail.com>
2022-02-10 14:08:33 +0100

    trying with playwright

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -3,20 +3,20 @@ import pytest
 from src.calculator import add
 from src.testrail import *
 
-client = APIClient('https://testautomationtest.testrail.io/')
-client.user = 'jonas.eichwald@velux.com'
-client.password = '6/FeVpnvRjU3TjdRFnkT'
+# client = APIClient('https://testautomationtest.testrail.io/')
+# client.user = 'jonas.eichwald@velux.com'
+# client.password = '6/FeVpnvRjU3TjdRFnkT'
 
 
 def test_add():
     result = add(3, 4)
     assert result == 7
-    res = client.send_post(
-       'add_result_for_case/29/2391',
-       {'status_id': 1, 'comment': 'This test worked fine!'}
-    )
+    # res = client.send_post(
+    #    'add_result_for_case/29/2391',
+    #    {'status_id': 1, 'comment': 'This test worked fine!'}
+    # )
 
-    print(res)
+    # print(res)
 
 
 def test_add_string():
--- tests/test_division.py
+++ tests/test_division.py
@@ -1,6 +1,6 @@
 from src.calculator import divide
 import pytest
-
+from playwright.sync_api import Page
 
 def test_divide():
     result = divide(3, 2)
@@ -15,3 +15,8 @@ def test_divide_by_zero():
 def test_divide_string():
     with pytest.raises(TypeError):
         divide("string", 2)
+
+def test_example_is_working(page: Page):
+    page.goto("https://example.com")
+    assert page.inner_text('h1') == 'Example Domain'
+    page.click("text=More information")
