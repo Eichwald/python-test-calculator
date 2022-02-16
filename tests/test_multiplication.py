5c919be6d8096ce36b748159e415b2f50ad48310
Eichwald <Eichwald6@hotmail.com>
2022-02-16 13:36:56 +0100

    merge conflict

--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -1,15 +1,9 @@
 from src.calculator import multiply
 import pytest
 
-
-
 def test_multiply():
     result = multiply(3, 4)
     assert result == 12
-<<<<<<< HEAD
-
-=======
->>>>>>> 1cbd41d3c8f1a53080735cb9fe68485f51fe4968
 
 
 def test_multiply_string():
