b0c5bdda1e2b8b54572f1e8c1af05128716b7b76
Eichwald <Eichwald6@hotmail.com>
2022-02-16 14:29:54 +0100

    without playwright

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -2,12 +2,10 @@ import pytest
 
 from src.calculator import add
 
-@pytest.mark.c0001
 def test_add():
     result = add(3, 4)
     assert result == 7
 
-@pytest.mark.c0002
 def test_add_string():
     with pytest.raises(TypeError):
         add("string", 4)
